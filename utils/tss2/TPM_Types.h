13627e2d7f63ef30918ee70f013ad004bfb4cc53
James Bottomley <jejb@linux.vnet.ibm.com>
2017-01-12 15:19:27 -0800

    allow the full range of session handles
    In the current code, session handles are only allowed in the range
    02000000-0200003f; 03000000-0300003f with TPM_RC_VALUE being given if a
    value is unmarshalled outside that range.  This rather conflicts with
    the embryonic Linux in-kernel Resource Manager because it virtualizes
    handles by counting down from the top of the handle range (to be as
    different as possible from physical handles).  This means that the TSS
    needs to allow the handle range to be 02000000-02ffffff; 03000000
    -03ffffff.  Fix that by changing the defines in TPM_Types.h
    
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- utils/tss2/TPM_Types.h
+++ utils/tss2/TPM_Types.h
@@ -738,11 +738,11 @@ typedef TPM_HANDLE TPM_RH;
 #define PCR_FIRST		(HR_PCR + 0)					/* first PCR */
 #define PCR_LAST		(PCR_FIRST + IMPLEMENTATION_PCR-1)		/* last PCR */
 #define HMAC_SESSION_FIRST	(HR_HMAC_SESSION + 0)				/* first HMAC session */
-#define HMAC_SESSION_LAST	(HMAC_SESSION_FIRST+MAX_ACTIVE_SESSIONS-1)	/* last HMAC session */
+#define HMAC_SESSION_LAST	(HMAC_SESSION_FIRST | 0x00FFFFFF)	/* last HMAC session */
 #define LOADED_SESSION_FIRST  	HMAC_SESSION_FIRST				/* used in GetCapability */
 #define LOADED_SESSION_LAST	HMAC_SESSION_LAST				/* used in GetCapability */
 #define POLICY_SESSION_FIRST	(HR_POLICY_SESSION + 0)				/* first policy session */
-#define POLICY_SESSION_LAST	(POLICY_SESSION_FIRST + MAX_ACTIVE_SESSIONS-1)	/* last policy session */
+#define POLICY_SESSION_LAST	(POLICY_SESSION_FIRST | 0x00FFFFFF)	/* last policy session */
 #define TRANSIENT_FIRST		((UINT32)(HR_TRANSIENT + 0))			/* first transient object */
 #define ACTIVE_SESSION_FIRST	POLICY_SESSION_FIRST				/* used in GetCapability */
 #define ACTIVE_SESSION_LAST	POLICY_SESSION_LAST				/* used in GetCapability */
