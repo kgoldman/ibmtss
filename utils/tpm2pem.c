7b6a37bfaa7ef257f4eb660d4eedeb294446c629
Ken Goldman <kgoldman@us.ibm.com>
2019-03-27 15:33:33 -0400

    UTILS: tpm2pem needs WIN32_LEAN_AND_MEAN
    To support Windows 10, whose crypto API clashes with openssl.
    
    Signed-off-by: Ken Goldman <kgoldman@us.ibm.com>

--- utils/tpm2pem.c
+++ utils/tpm2pem.c
@@ -43,6 +43,13 @@
 #include <string.h>
 #include <stdint.h>
 
+/* Windows 10 crypto API clashes with openssl */
+#ifdef TPM_WINDOWS
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#endif
+
 #include <ibmtss/tsserror.h>
 #include <ibmtss/tssutils.h>
 #include <ibmtss/tsscrypto.h>
