bdd0fcd3affa62d4e091677e97140939fc797ea3
Mauro S. M. Rodrigues <maurosr@linux.vnet.ibm.com>
2019-11-25 12:03:48 -0300

    TSS/Eventlog: Adding multiple helpers for eventlib
    This introduces multiple functions to help and deal with Eventlog data,
    specifically for structures that belong to event data but also to other
    structures, and thus it's adding similar code to deal with the little
    endian used when marshaling/unmarshaling eventlog data to/from memory
    buffer.
    
    For instance. PCR extension operations don't use a specfic
    endianess, but also marshall/unmarshal Algorithm ID, which is also present
    at TSS_Event2 which needs to be LE when marshalling and unmarshalling
    
    Signed-off-by: Mauro S. M. Rodrigues <maurosr@linux.vnet.ibm.com>
    Signed-off-by: Ken Goldman <kgoldman@us.ibm.com>

--- utils/eventlib.h
+++ utils/eventlib.h
@@ -3,9 +3,8 @@
 /*		     	TPM2 Measurement Log Common Routines			*/
 /*			     Written by Ken Goldman				*/
 /*		       IBM Thomas J. Watson Research Center			*/
-/*	      $Id: eventlib.h 1257 2018-06-27 20:52:08Z kgoldman $		*/
 /*										*/
-/* (c) Copyright IBM Corporation 2016, 2017.					*/
+/* (c) Copyright IBM Corporation 2016 - 2020.					*/
 /*										*/
 /* All rights reserved.								*/
 /* 										*/
@@ -155,6 +154,8 @@ extern "C" {
     
     TPM_RC TSS_EVENT_Line_Unmarshal(TCG_PCR_EVENT *event, BYTE **buffer, uint32_t *size);
 
+    TPM_RC TSS_EVENT_Line_LE_Unmarshal(TCG_PCR_EVENT *target, BYTE **buffer, uint32_t *size);
+
     TPM_RC TSS_EVENT_PCR_Extend(TPMT_HA pcrs[IMPLEMENTATION_PCR],
 				TCG_PCR_EVENT *event);
     
@@ -167,7 +168,15 @@ extern "C" {
     TPM_RC TSS_EVENT2_Line_Marshal(TCG_PCR_EVENT2 *source, uint16_t *written,
 				   uint8_t **buffer, uint32_t *size);
 
+    TPM_RC TSS_EVENT2_Line_LE_Marshal(TCG_PCR_EVENT2 *source, uint16_t *written,
+				   uint8_t **buffer, uint32_t *size);
+
+
     TPM_RC TSS_EVENT2_Line_Unmarshal(TCG_PCR_EVENT2 *target, BYTE **buffer, uint32_t *size);
+
+    TPM_RC TSS_EVENT2_Line_LE_Unmarshal(TCG_PCR_EVENT2 *target, BYTE **buffer, uint32_t *size);
+
+
 
     TPM_RC TSS_EVENT2_PCR_Extend(TPMT_HA pcrs[HASH_COUNT][IMPLEMENTATION_PCR],
 				 TCG_PCR_EVENT2 *event2);
@@ -181,6 +190,12 @@ extern "C" {
     void TSS_SpecIdEvent_Trace(TCG_EfiSpecIDEvent *specIdEvent);
 
     const char *TSS_EVENT_EventTypeToString(uint32_t eventType);
+
+    TPM_RC TSS_UINT32LE_Marshal(const UINT32 *source, uint16_t *written,
+                                BYTE **buffer, uint32_t *size);
+
+    TPM_RC TSS_UINT16LE_Marshalu(const UINT16 *source, uint16_t *written,
+                                 BYTE **buffer, uint32_t *size);
 
 #ifdef __cplusplus
 }
